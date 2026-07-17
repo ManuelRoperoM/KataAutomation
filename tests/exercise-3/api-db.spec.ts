5dbf0be6ab3dcd7329b93a205e26abb2bc552db6
ManuelR <mnlrpr@gmail.com>
2026-07-17 10:43:38 -0500

    Completed part 3

--- tests/exercise-3/api-db.spec.ts
+++ tests/exercise-3/api-db.spec.ts
@@ -1,9 +1,49 @@
 import { test, expect } from '@playwright/test';
 import { login, createTodo } from './api-helpers';
-import { isTodoCompleted } from './db-helper';
+import { isTodoCompleted, closeConnection, cleanupTestTodos } from './db-helper';
 
-test('todo created via API', async ({ page, request }) => {
-    const title = `TEST-${crypto.randomUUID()}`;
+
+let titleTodo: string;
+
+
+test.describe('Connection with DataBase and api', () => {
+  test.beforeEach(() => {
+    titleTodo = `TEST-${crypto.randomUUID()}`;
+  });
+  
+  test.afterEach(async () => {
+    await cleanupTestTodos(titleTodo);
+  });
+  
+  test.afterAll(async () => {
+    await closeConnection();
+  });
+  // PRIMERA PARTE
+  test('todo created via API', async ({ page, request }) => {
+    
+      const auth = await login(
+        request,
+        'user@test.com',
+        'Test1234!'
+      );
+    
+      await createTodo(request, auth.token, titleTodo);
+    
+      await page.addInitScript(({ token, userName }) => {
+        localStorage.setItem('token', token);
+        localStorage.setItem('userName', userName);
+      }, {
+        token: auth.token,
+        userName: auth.user.name,
+      });
+    
+      await page.goto('/todos');
+    
+      await expect(page.getByText(titleTodo)).toBeVisible();
+  });
+  
+  // SEGUNDA PARTE
+  test('Completed todo in database', async ({ page, request }) => {
   
     const auth = await login(
       request,
@@ -11,62 +51,37 @@ test('todo created via API', async ({ page, request }) => {
       'Test1234!'
     );
   
-    await createTodo(request, auth.token, title);
+    const todo = await createTodo(
+        request,
+        auth.token,
+        titleTodo
+    );
   
-    await page.addInitScript(({ token, userName }) => {
-      localStorage.setItem('token', token);
-      localStorage.setItem('userName', userName);
-    }, {
-      token: auth.token,
-      userName: auth.user.name,
-    });
+    await page.addInitScript(
+        ({ token, userName }) => {
+            localStorage.setItem('token', token);
+            localStorage.setItem('userName', userName);
+        },
+        {
+            token: auth.token,
+            userName: auth.user.name,
+        }
+    );
   
     await page.goto('/todos');
   
-    await expect(page.getByText(title)).toBeVisible();
-});
-
-
-test('Completed todo in database', async ({ page, request }) => {
-
-  const title = `TEST-${crypto.randomUUID()}`;
-
-  const auth = await login(
-    request,
-    'user@test.com',
-    'Test1234!'
-  );
-
-  const todo = await createTodo(
-      request,
-      auth.token,
-      title
-  );
-
-  await page.addInitScript(
-      ({ token, userName }) => {
-          localStorage.setItem('token', token);
-          localStorage.setItem('userName', userName);
-      },
-      {
-          token: auth.token,
-          userName: auth.user.name,
-      }
-  );
-
-  await page.goto('/todos');
-
-  const todoItem = page
-      .getByTestId('todo-item')
-      .filter({
-          has: page.getByText(title, { exact: true }),
-      });
-
-  await todoItem
-      .locator('input[type="checkbox"]')
-      .check();
-
-  const completed = await isTodoCompleted(todo.id);
-
-  expect(completed).toBe(true);
-});
+    const todoItem = page
+        .getByTestId('todo-item')
+        .filter({
+            has: page.getByText(titleTodo, { exact: true }),
+        });
+  
+    await todoItem
+        .locator('input[type="checkbox"]')
+        .check();
+  
+    const completed = await isTodoCompleted(todo.id);
+  
+    expect(completed).toBe(true);
+  });
+})
